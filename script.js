4da6a7a9b281b22bd21cb52fd27add6a63ba8f23
nicouchimura <nicolasruchimura@gmail.com>
2026-05-27 18:00:48 +0100

    feat: javaScript + functional calculator

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
 const display = document.getElementById("display");
 
 function appendToDisplay(input){
-
+    display.value += input;
+    adjustFontSize();
 }
 
 function clearDisplay(){
-
+    display.value="";
+    adjustFontSize();
 }
 
 function calculate(){
-    
+    try{
+        let expressao = display.value.replace(/x/g, '*');
+        display.value = eval(expressao);
+    } 
+    catch(error){
+        display.value="Error";
+    }
+    adjustFontSize();
+}
+
+function adjustFontSize(){
+    const totalChar = display.value.length;
+
+    if(totalChar>12){
+        display.style.fontSize="1.5rem";
+    } else if(totalChar>8){
+        display.style.fontSize="2.2rem";
+    } else {
+        display.style.fontSize="3rem";
+    }
 }
